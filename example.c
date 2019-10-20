1889e95680cab2f736aec87372affa6358c09f36
JornVeken <51487819+JornVeken@users.noreply.github.com>
2019-10-20 16:38:33 +0200

    Added comments

--- example.c
+++ example.c
@@ -4,9 +4,11 @@
 #define HEIGHT 12
 
 int main() {
+    /*Creating display buffers*/
     display* canvas = create_display(WIDTH, HEIGHT);
     display* sprite = create_display(10 , 10);
 
+    /*Drawing onto the sprite buffer*/
     set_rect(sprite, 1, 1, 10, 10, 1);
     set_rect(sprite, 2, 2,  8,  8, 0);
     set_line(sprite, 3, 8, 8, 8, 1);
@@ -15,9 +17,13 @@ int main() {
     set_rect(sprite, 3, 3, 2, 2, 1);
     set_rect(sprite, 7, 3, 2, 2, 1);
 
+    /*Drawing the sprite onto the canvas*/
     set_section(canvas, 2, 2, sprite);
 
+    /*Printing and cleaning up*/
     print_display(canvas, 1);
     free_display(canvas);
+    free_display(sprite);
+    
     return 0;
 }
